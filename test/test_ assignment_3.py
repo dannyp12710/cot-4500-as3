b4e39b4934ac4633b4f17b0284ca6600a280d203
dannyp12710 <danielpinilla280@gmail.com>
2025-03-07 15:26:08 -0500

    Update test_ assignment_3.py

--- test/test_ assignment_3.py
+++ test/test_ assignment_3.py
@@ -1,1 +1,14 @@
+# For Question 1 you will use the Euler Method with the following 
 
+# a. Function: t – y 2
+# b. Range: 0 < t < 2
+# c. Iterations: 10
+# d. Initial Point: f(0) = 1
+
+
+# For Question 2 you will use the Runge-Kutta Method with the following 
+
+# a. Function: t – y 2
+# b. Range: 0 < t < 2
+# c. Iterations: 10
+# d. Initial Point: f(0) = 1
